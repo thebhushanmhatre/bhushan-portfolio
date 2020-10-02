96f3faca63d4ef49d56fb1e9b0fe3e3099424b00
Bhushan C. Mhatre <44663470+thebhushanmhatre@users.noreply.github.com>
2020-10-02 18:07:22 +0530

    Added Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,13 @@
-import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+import React from 'react';
+import { Container } from 'reactstrap';
 
-function RenderMedia({item}) {
+function Home(){
   return(
-    <div className="mb-4 pt-2 col-12 offset-md-1 col-md-9 col-lg-8 educationMedia" 
-      style={{backgroundColor: window.innerWidth < 995 ? 'white' : ''}}>
-      <Media className="pb-2">
-        <Media body>
-          <Media heading>{item.name}</Media>
-          <Media heading>{item.shortname}</Media>
-          <p>{item.branch}</p>
-          <p> Graduated in {item.year}</p>
-        </Media>
-        <div className="d-none d-sm-block" >
-        <a href={item.href} target="_blank" rel="noopener noreferrer">
-          <Media style={{borderRadius: '50px'}} object src={item.src} alt={item.name} width="100px" height="120px" />
-        </a>
-        </div>
-      </Media>
-    </div>
+    <Container className="text-center">
+      <img className="mb-4" src="/assets/images/onclouds.jpg" alt="Bhushan Mhatre" style={{borderRadius: '50%'}}/>
+      <h2><i>Bhushan Chandrakant Mhatre</i></h2>
+    </Container>
   );
-}
-
-class Home extends Component{
-
-  render(){
-    const institutes = this.props.education.map(item=>
-        <RenderMedia item={item} />
-    )
-    return(
-      <div className="container">
-        <h2><span className="fa fa-institution fa-md mb-4"> Education</span></h2>
-        {institutes}
-      </div>
-    );
-  }
 }
 
 export default Home;
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function WorkExperience(){
   return(
-    <div className="container text-center mt-5 p-2" style={{textShadow: '1px 1px 10px white', opacity: 1, backgroundColor: 'whitesmoke'}}>
+    <div className="container text-center mt-5 p-3" style={{textShadow: '1px 1px 10px white', opacity: 1, backgroundColor: 'whitesmoke'}}>
       <img className="col-12 col-md-8 col-lg-4" src="assets/images/MSCI_logo.svg" alt="MSCI Logo from Wikipedia"/>
       <h3>ESG Department</h3>
       <i style={{fontSize: '1.12em'}}><strong>Software Developer</strong> - Ruby, Javascript</i>
       <br/>
-      <i style={{fontSize: '1.12em'}}>10th June, 2019 to Present</i>
+      <i style={{fontSize: '1.12em'}}>June 2019 to Present</i>
     </div>
   );
 }
